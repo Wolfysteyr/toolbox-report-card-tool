f2a736bb9a6b6190be32df99b33d06ff3861553c
Maksims Carevs <mca@vtl.lv>
2026-04-16 15:17:21 +0300

    Added migration to change type for motora_tilpums column

---
+++ database/migrations/2026_04_16_121449_change_motora_tilpums_to_string_in_energy_locals.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('energy_local', function (Blueprint $table) {
+            Schema::table('energy_local', function (Blueprint $table) {
+                $table->string('motora_tilpums')->nullable()->change();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('energy_local', function (Blueprint $table) {
+            //
+        });
+    }
+};
